332dc448af9587fc338ecc7fa0b38652bb010ddb
proav2323 <108835201+proav2323@users.noreply.github.com>
2023-12-16 21:09:47 -0600

    coding

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import { Toaster } from '@/components/ui/toaster'
+import { ThemeProvider } from '@/components/providers/ThemeProviders'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,7 +18,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <ThemeProvider>
+        <body className={inter.className}>{children}</body>
+        <Toaster />
+      </ThemeProvider>
     </html>
   )
 }
