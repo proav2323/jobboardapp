c741c70c3436ea5e0f3173aab50909ac382da334
proav2323 <108835201+proav2323@users.noreply.github.com>
2024-01-06 13:05:16 -0600

    cdoing

--- components/models/AddJob.tsx
+++ components/models/AddJob.tsx
@@ -27,7 +27,7 @@ export default function AddJob() {
 
   return (
   <Dialog open={isOpen} onOpenChange={() => model.onClose()}>
-   <DialogContent className=' overflow-y-auto'>
+   <DialogContent className=' overflow-y-scroll'>
     <Heading title={isEditingJob === true ? `Edit Job ${job?.title}` :'Add A New Job'} subtitle={isEditingJob === true ? "need to edit job?" :'need to hire someone for partiular job?'} center />
        <AddJobForm model={model} isEditing={isEditingJob} job={job} />
     </DialogContent>
